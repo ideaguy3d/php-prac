e801451a4f9901cdff42fc9209c2f562fafc4596
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-07 23:11:37 -0800

    added angular routing with req to php backend

--- app2/app.js
+++ app2/app.js
@@ -2,17 +2,45 @@
  * Created by Julius Alvarado on 3/7/2017.
  */
 (function () {
-    var app = angular.module('app', []);
+    var app = angular.module('app', ['ngRoute']);
 
     app.factory('jDataService', ['$http',
         function ($http) {
             var baseUrl = 'services';
 
             return {
-                getCountries: $http.get(baseUrl+'/getCountries.php')
+                getCountries: $http.get(baseUrl+'/getCountries.php'),
+                getStates: function(countryCode){
+                    return $http.get(baseUrl+'/getStates.php?countryCode='+
+                        encodeURIComponent(countryCode));
+                }
             }
         }
     ]);
+
+    app.config(function($routeProvider){
+        $routeProvider.when('/states/:countryCode', {
+            templateUrl: 'state.view.html',
+            controller: function($routeParams, jDataService){
+                var vm = this;
+                vm.newState = '';
+                vm.params = $routeParams;
+
+                jDataService.getStates(vm.params.countryCode || '')
+                    .then(function(res){
+                        vm.states = res.data;
+                    }
+                );
+
+                vm.addStateTo = function(){
+                    if(!vm.states) vm.states = [];
+                    vm.states.push({name: vm.newState});
+                    vm.newState = '';
+                }
+            },
+            controllerAs: 'stateCtrl'
+        });
+    });
 
     app.controller('CountryCtrl', ['jDataService',
         function (jDataService) {
