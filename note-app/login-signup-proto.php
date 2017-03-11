11e34f6c1edcd6defe825a82ce903ef5d1065077
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-11 13:11:54 -0800

    initial design coded

--- note-app/login-signup-proto.php
+++ note-app/login-signup-proto.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 3/11/2017
+ * Time: 9:43 AM
+ */
+
 session_start();
 $error = '';
 
@@ -6,7 +13,7 @@ if (array_key_exists("logout", $_GET)) {
     unset($_SESSION);
     setcookie('id', '', time() - 60 * 60);
     $_COOKIE['id'] = '';
-} else if ((array_key_exists('id', $_SESSION) AND $_SESSION['id'])
+} else if ( (array_key_exists('id', $_SESSION) AND $_SESSION['id'])
     OR (array_key_exists('id', $_COOKIE) AND $_COOKIE['id'])
 ) {
     header('Location: logged-in-page.php');
@@ -54,7 +61,7 @@ if (array_key_exists('email', $_POST)) {
             #endregion
         } else {
             $email = mysqli_real_escape_string($link, $_POST['email']);
-            $query = 'select * from `users` where email="' . $email . '"';
+            $query = 'select * from `users` where email="'.$email.'"';
             $result = mysqli_query($link, $query);
             $row = mysqli_fetch_array($result); //
             if (isset($row)) {
@@ -81,25 +88,14 @@ if (array_key_exists('email', $_POST)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>www.julius3d.com</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
           integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
     <style>
-        html {
-            background: url(bg1280.jpg) no-repeat center center fixed;
-            background-size: cover;
-        }
-
-        body {
-            background: none;
-            color: whitesmoke;
-        }
-
         .jcol {
             float: left;
             width: 50%;
@@ -112,10 +108,6 @@ if (array_key_exists('email', $_POST)) {
         #error {
             color: indianred;
         }
-
-        .display {
-            display: none;
-        }
     </style>
 </head>
 
@@ -123,74 +115,27 @@ if (array_key_exists('email', $_POST)) {
 <br>
 <hr>
 <section class="container">
-    <div class="text-center">
-        <h1>Note Taking Application</h1>
-        <h5>The internets best note taking app. Give it a try and Find out for yourself!</h5>
-    </div>
+    <h1>Sign up or Login Now</h1>
     <div id="error"><?= $error; ?></div>
-    <!-- Sign up form: -->
-    <form id="signup-form" method="post">
-        <h1>Sign up</h1>
-        <fieldset class="form-group">
-            <input id="email" class="form-control" type="email" name="email" placeholder="email">
-        </fieldset>
-        <fieldset class="form-group">
-            <input id="password" class="form-control" type="password" name="password" placeholder="password">
-        </fieldset>
-        <div class="checkbox">
-            <label for="stayLoggedIn">
-                Stay logged in? <input id="stayLoggedIn" type="checkbox" name="stayLoggedIn" value=1>
-            </label>
-        </div>
-        <fieldset class="form-group">
-            <input type="hidden" name="signUp" value="1">
-            <input type="submit" name="submit" class="btn btn-primary" value="Sign Up">
-        </fieldset>
-        <p><a class="switch-form btn btn-success btn-sm">Or Login</a></p>
+    <!-- Sign up section: -->
+    <form action="" method="post">
+        <input id="email" type="email" name="email" placeholder="email">
+        <input id="password" type="password" name="password" placeholder="password">
+        <label for="stayLoggedIn">Stay logged in?</label>
+        <input id="stayLoggedIn" type="checkbox" name="stayLoggedIn" value=1>
+        <input type="hidden" name="signUp" value="1">
+        <input type="submit" name="submit" class="btn btn-primary" value="Sign Up">
     </form>
-    <!-- Login form: -->
-    <form id="login-form" class="display" method="post">
-        <h1>Login</h1>
-        <fieldset class="form-group">
-            <input id="email" class="form-control" type="email" name="email" placeholder="email">
-        </fieldset>
-        <fieldset class="form-group">
-            <input id="password" class="form-control" type="password" name="password" placeholder="password">
-        </fieldset>
-        <fieldset class="form-group">
-            <label for="stayLoggedIn">Stay logged in?</label>
-            <input id="stayLoggedIn" type="checkbox" name="stayLoggedIn" value=1>
-        </fieldset>
-        <fieldset class="form-group">
-            <input type="hidden" name="signUp" value="0">
-            <input type="submit" name="submit" class="btn btn-primary" value="Login">
-        </fieldset>
-        <p><a class="switch-form btn btn-success btn-sm">Or Sign up</a></p>
+    <br>
+    <!-- Login in section: -->
+    <form method="post">
+        <input id="email" type="email" name="email" placeholder="email">
+        <input id="password" type="password" name="password" placeholder="password">
+        <label for="stayLoggedIn">Stay logged in?</label>
+        <input id="stayLoggedIn" type="checkbox" name="stayLoggedIn" value=1>
+        <input type="hidden" name="signUp" value="0">
+        <input type="submit" name="submit" class="btn btn-primary" value="Login">
     </form>
 </section>
-
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
-        integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
-        integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
-        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
-        crossorigin="anonymous"></script>
-<script type="text/javascript">
-    (function ($) {
-        $('.switch-form').click(function () {
-            $('#signup-form').toggle('display');
-            $('#login-form').toggle('display');
-        });
-    }(jQuery));
-</script>
 </body>
 </html>
-
-
-
-
-
