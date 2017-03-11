13a3eb15abb37decc8a0e951382dea742366ac56
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-11 09:33:42 -0800

    finished login/signup

--- note-app/index.php
+++ note-app/index.php
@@ -6,13 +6,15 @@ if (array_key_exists("logout", $_GET)) {
     unset($_SESSION);
     setcookie('id', '', time() - 60 * 60);
     $_COOKIE['id'] = '';
-} else if (array_key_exists('id', $_SESSION) OR array_key_exists('id', $_COOKIE)) {
+} else if ( (array_key_exists('id', $_SESSION) AND $_SESSION['id'])
+    OR (array_key_exists('id', $_COOKIE) AND $_COOKIE['id'])
+) {
     header('Location: logged-in-page.php');
 }
 
 if (array_key_exists('email', $_POST)) {
     $link = mysqli_connect('127.0.0.1', 'root', '', 'users');
-    if (mysqli_connect_error()) die (' There has been a Database connection error :(');
+    if (mysqli_connect_error()) die ('There has been a Database connection error :(');
 
     if (!$_POST['email']) {
         $error .= 'Email is required<b>';
@@ -23,48 +25,53 @@ if (array_key_exists('email', $_POST)) {
     if ($error != '') {
         $error = '<p>There were errors: </p>' . $error;
     } else { // there were no errors signing up
-        if($_POST['signUp'] == '1') { // user is signing up
-        #region //-- signing up code:
-        $email = mysqli_real_escape_string($link, $_POST['email']);
-        $password = mysqli_real_escape_string($link, $_POST['password']);
-        $query = "select `id` from `users` where email = '$email' limit 1";
-        $result = mysqli_query($link, $query);
+        if ($_POST['signUp'] == '1') { // user is signing up
+            #region //-- signing up code:
+            $email = mysqli_real_escape_string($link, $_POST['email']);
+            $password = mysqli_real_escape_string($link, $_POST['password']);
+            $query = "select `id` from `users` where email = '$email' limit 1";
+            $result = mysqli_query($link, $query);
 
-        if (mysqli_num_rows($result) > 0) {
-            $error = 'That email is already taken';
-        } else {
-            $query = "insert into users (email, password) VALUES ('$email', '$password')";
-            if (!mysqli_query($link, $query)) {
-                $error .= 'Error signing you up. Please try again.';
+            if (mysqli_num_rows($result) > 0) {
+                $error = 'That email is already taken';
             } else {
-                $recentId = mysqli_insert_id($link); // gets id of most recently inserted record
-                $hashedPassword = md5(md5(mysqli_insert_id($link)) . $_POST['password']);
-                $query = 'update users set password = "$hashedPassword" where id = "$recentId" limit 1';
-                mysqli_query($link, $query);
-                $_SESSION['id'] = $recentId;
-                if ($_POST['stayLoggedIn'] == '1') {
-                    setcookie("id", $recentId, time() + 60 * 60 * 24);
+                $query = "insert into users (email, password) VALUES ('$email', '$password')";
+                if (!mysqli_query($link, $query)) {
+                    $error .= 'Error signing you up. Please try again.';
+                } else {
+                    $recentId = mysqli_insert_id($link); // gets id of most recently inserted record
+                    $hashedPassword = md5(md5(mysqli_insert_id($link)) . $_POST['password']);
+                    $query = 'update users set password = "$hashedPassword" where id = "$recentId" limit 1';
+                    mysqli_query($link, $query);
+                    $_SESSION['id'] = $recentId;
+                    if ($_POST['stayLoggedIn'] == '1') {
+                        setcookie("id", $recentId, time() + 60 * 60 * 24);
+                    }
+                    //logged-in-page.php
+                    header('Location: logged-in-page.php');
                 }
-                //logged-in-page.php
-                header('Location: logged-in-page.php');
             }
-        }
-        #endregion
+            #endregion
         } else {
             $email = mysqli_real_escape_string($link, $_POST['email']);
-            $query = 'select * from users where email="$email" limit 1';
+            $query = 'select * from `users` where email="'.$email.'"';
             $result = mysqli_query($link, $query);
-            $row = mysqli_fetch_array($result);
-            if(array_key_exists('id', $row)) {
-                $hashedPassword = md5(md5($row['id']).$_POST['password']);
-                if($hashedPassword == $row['password']) {
+            $row = mysqli_fetch_array($result); //
+            if (isset($row)) {
+                echo 'in password check region';
+                // $hashedPassword = md5(md5($row['id']) . $_POST['password']); // this doesn't work :(
+                $password = mysqli_real_escape_string($link, $_POST['password']);
+                if ($password == $row['password']) {
                     $_SESSION['id'] = $row['id'];
-                    if($_POST['stayLoggedIn'] == '1') {
-                        setcookie('id', $row['id'], time() * 60 * 60));
+                    if ($_POST['stayLoggedIn'] == '1') {
+                        setcookie('id', $row['id'], time() * 60 * 60);
                     }
+                    header('Location: logged-in-page.php');
                 } else {
                     echo 'jha - passwords did not match';
                 }
+            } else {
+                $error .= 'that email/password combination could not be found';
             }
         }
     }
